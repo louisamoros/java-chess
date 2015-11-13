a0807da6dd1ed616182bfc388a0d2c4eab702f29
guillaume Nostrenoff <g.nostrenoff@gmail.com>
2015-11-13 17:02:18 +0100

    ChessGame implemented

--- chess/src/model/observable/ChessGame.java
+++ chess/src/model/observable/ChessGame.java
@@ -1,5 +1,46 @@
 package model.observable;
 
-public class ChessGame {
-	//TODO
+import java.util.Observable;
+
+import controler.controlerLocal.ChessGameControler;
+import vue.ChessGameCmdLine;
+import model.Echiquier;
+
+public class ChessGame extends Observable{
+	
+	private static boolean isInstance;
+	private Echiquier echiquier;
+	
+	private ChessGame(){
+		super();
+		//creation de l'echiquier
+		echiquier = new Echiquier();
+	}
+	
+	//make sure we have only one instance of Chessgame
+	public ChessGame getInstance(){
+		if(!isInstance){
+			isInstance = true;
+			return new ChessGame();
+		}
+		else
+			return null;
+	}
+	
+	/**
+	 * vérifie si le déplacement est possible
+	 * effectue le déplacement s’il est possible
+	 * effectue aussi le changement de joueur si le déplacement est OK
+	 */
+	public boolean move(int xInit, int yInit, int xFinal, int yFinal){
+		if(echiquier.isMoveOk(xInit, yInit, xFinal, yFinal))
+			if(echiquier.move(xInit, yInit, xFinal, yFinal))
+				return true;
+		return false;
+	}
+	
+	public String toString(){
+		//todo
+		return echiquier.toString();
+	}
 }
