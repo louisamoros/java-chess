8486cf478c7db12c88155259221056910ef79a12
louis <amoros.louis@gmail.com>
2015-11-25 11:42:39 +0100

    LauncherGUIClient and Server created.

---
+++ chess/src/launcher/localLauncher/LauncherGUIServer.java
@@ -0,0 +1,30 @@
+package launcher.localLauncher;
+
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+import model.observable.ChessGame;
+import vue.ChessGameGUI;
+import controler.controlerLocal.ChessGameControler;
+
+public class LauncherGUIServer {
+	public static void main(String[] args) {
+
+		ChessGame chessGame;
+		chessGame = ChessGame.getInstance();
+
+		ChessGameControler chessGameControler = new ChessGameControler(
+				chessGame);
+		JFrame frame = new ChessGameGUI(chessGameControler);
+
+		chessGame.addObserver((Observer) frame);
+		chessGame.notifyObservers();
+
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+		
+		//Create socket
+	}
+}
